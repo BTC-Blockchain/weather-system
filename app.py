81a8caeb58e835cdd329dc133b0439f95a83ffd1
BTC-Blockchain <61820663+BTC-Blockchain@users.noreply.github.com>
2026-03-25 17:41:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -359,7 +359,7 @@ with col2:
 
     st.markdown(f"**METAR最新发布：{formatted_time}**")
 
-    st.info("📌 当系统启动运行时会自动获取当天0点开始的历史数据以填充温度曲线图")
+    st.info("📌 当系统启动运行将自动获取当天0点开始的历史数据以填充温度图")
     st.markdown("### 📈 温度曲线")
     df = pd.DataFrame(data)
     df["time"] = pd.to_datetime(df["time"])
