2f66fb1d6a8b7ca1ec2bb843f0d7be427b809013
BTC-Blockchain <61820663+BTC-Blockchain@users.noreply.github.com>
2026-04-01 21:07:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -550,9 +550,9 @@ data = load_cache()
 # 只要少于 5 条，就认为历史数据补全失败，强制再运行一次
 # if len(data) < 1:
     # 强制在控制台和网页同时输出，确保你能看到
-    print(f"📡 [DEBUG] 当前缓存数据量 {len(data)} 条，开始执行 init_today_history...")
-    st.toast("正在尝试补全今日历史报文...", icon="🔄", duration=10)
-    data = init_today_history()
+print(f"📡 [DEBUG] 当前缓存数据量 {len(data)} 条，开始执行 init_today_history...")
+st.toast("正在尝试补全今日历史报文...", icon="🔄", duration=10)
+data = init_today_history()
 
 # 获取实时数据
 data, is_new, source = get_today_data()
